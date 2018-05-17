9eb21392f7d7260e99b0afa893a4b26bf636192e
Akron <nils@diewald-online.de>
2018-05-17 22:08:26 +0200

    Improve support for recent versions of Mojolicious

--- lib/XML/Loy.pm
+++ lib/XML/Loy.pm
@@ -5,7 +5,7 @@ use Carp qw/croak carp/;
 use Scalar::Util qw/blessed weaken/;
 use Mojo::Base 'Mojo::DOM';
 
-our $VERSION = '0.46';
+our $VERSION = '0.47';
 
 sub DESTROY;
 
@@ -1443,7 +1443,7 @@ L<Renée Bäcker|https://github.com/reneeb>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2011-2016, L<Nils Diewald|http://nils-diewald.de/>.
+Copyright (C) 2011-2018, L<Nils Diewald|http://nils-diewald.de/>.
 
 This program is free software, you can redistribute it
 and/or modify it under the same terms as Perl.
